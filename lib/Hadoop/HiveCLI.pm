8645746413c35ba1124c0ee3aabe7ded162c2cda
Stephen R. Scaffidi <sscaffidi@gmail.com>
2012-08-27 12:48:00 -0400

    initial commit

---
+++ lib/Hadoop/HiveCLI.pm
@@ -0,0 +1,19 @@
+use strict;
+use warnings;
+package Hadoop::HiveCLI;
+# ABSTRACT: ???
+use Carp;
+use autodie;
+use Data::Dumper;
+use English qw(-no_match_vars);
+
+
+
+
+1 && q{ this statement is true }; # truth
+__END__
+
+=head1 DESCRIPTION
+
+=head1 SYNOPSIS
+
